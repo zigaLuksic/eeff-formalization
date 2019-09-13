c059a41c0962124e24632d348a311febd36e4f25
zigaLuksic <ziga.luksic@fmf.uni-lj.si>
2019-09-13 18:37:06 +0200

    Moving to de Brujin

--- language_formalisation/syntax/syntax.v
+++ language_formalisation/syntax/syntax.v
@@ -1,14 +1,29 @@
-Require Export Bool ZArith Int String.
+Require Export Bool ZArith Int String List.
 
-Definition var_id := string.
-Definition op_id := string.
+Import ListNotations.
+
+Definition var_id :=prod string nat.
+Definition id_correct (id : var_id) :=
+  let (idname, idnum) := id in
+  Nat.eqb idnum 0.
+Definition id_reduce (id : var_id) :=
+  let (idname, idnum) := id in
+  (idname, idnum - 1).
+Definition id_shift (id : var_id) d c :=
+  let (idname, idnum) := id in
+  if Nat.ltb idnum c then id else (idname, idnum + d).
+Definition id_same (id1 : var_id) (id2 : var_id) :=
+  let (idname1, idnum1) := id1 in
+  let (idname2, idnum2) := id2 in
+  Nat.eqb idnum1 idnum2.
 Definition int := Z.t.
-Definition eq_id := string_dec.
-Notation "x == y" := (eq_id x y) (at level 75, no associativity).
+Definition op_id := string.
+Definition eq_op_id := string_dec.
+Notation "x == y" := (eq_op_id x y) (at level 75, no associativity).
 Check "" == "".
 
 Inductive val : Type :=
-| Var : string -> val
+| Var : var_id -> val
 | Unit : val
 | Int : Z.t -> val
 | Inl : val -> val
@@ -17,9 +32,10 @@ Inductive val : Type :=
 | Fun : var_id -> comp -> val
 | Handler : var_id -> comp -> hcases -> val
 | VAnnot : val -> vtype -> val
+
 with comp : Type :=
 | Ret : val -> comp
-| ΠMatch : val -> var_id * var_id -> comp -> comp
+| ΠMatch : val -> comp -> comp
 | ΣMatch : val -> var_id -> comp -> var_id -> comp -> comp
 | App : val -> val -> comp
 | Op : op_id -> val -> var_id -> comp -> comp
@@ -27,9 +43,11 @@ with comp : Type :=
 | DoBind : var_id -> comp -> comp -> comp
 | Handle : val -> comp -> comp
 | CAnnot : comp -> ctype -> comp
+
 with hcases : Type :=
 | CasesØ : hcases
 | CasesU : hcases -> op_id -> var_id -> var_id -> comp -> hcases
+
 with vtype : Type :=
 | TyUnit : vtype
 | TyInt : vtype
@@ -38,39 +56,45 @@ with vtype : Type :=
 | TyΠ : vtype -> vtype -> vtype
 | TyFun : vtype -> ctype -> vtype
 | TyHandler : ctype -> ctype -> vtype
+
 with ctype : Type :=
 | CTy : vtype -> sig -> eqs -> ctype
+
 with sig : Type :=
 | SigØ : sig
-| SigU : op_id -> vtype -> vtype -> sig -> sig
+| SigU : sig -> op_id -> vtype -> vtype -> sig
+
 with ctx : Type :=
 | CtxØ : ctx
-| CtxU : var_id -> vtype -> ctx -> ctx
+| CtxU : ctx -> vtype -> ctx
+
 with tmpl_ctx : Type :=
 | TctxØ : tmpl_ctx
-| TctxU : var_id -> vtype -> tmpl_ctx -> tmpl_ctx
+| TctxU : tmpl_ctx -> vtype -> tmpl_ctx
+
 with tmpl : Type :=
 | TApp : var_id -> val -> tmpl
 | TΠmatch : val -> var_id -> var_id -> tmpl -> tmpl
 | TΣmatch : val -> var_id -> tmpl -> var_id -> tmpl -> tmpl
 | TOp : op_id -> val -> var_id -> tmpl -> tmpl
-with eqs : Type :=
+
+with eqs : Type := 
 | EqsØ : eqs
-| EqsU : ctx -> tmpl_ctx -> tmpl -> tmpl -> eqs
+| EqsU : eqs -> ctx -> tmpl_ctx -> tmpl -> tmpl -> eqs
 .
 
 Fixpoint get_vtype (Γ : ctx) (id : var_id) : option vtype :=
   match Γ with
   | CtxØ => None
-  | CtxU id' α Γ' =>
-      if id == id' then Some α
-      else get_vtype Γ' id
+  | CtxU Γ' α =>
+      if id_correct id then Some α
+      else get_vtype Γ' (id_reduce id)
   end.
 
 Fixpoint get_op_type (Σ : sig) (op : op_id) : option (vtype * vtype) :=
   match Σ with
   | SigØ => None
-  | SigU op' α β Σ' =>
+  | SigU Σ' op' α β =>
       if op == op' then Some (α, β)
       else get_op_type Σ' op
   end.
@@ -84,59 +108,41 @@ Fixpoint find_op_case (h : hcases) (op : op_id)
       else find_op_case h_others op
   end.
 
-(* NOT CAPTURE AVOIDING!!! *)
-Fixpoint vsubst (σ : var_id * val) (v : val) :=
-  (* v [id |-> sv] *)
-  let (s_id, s_v) := σ in
+Fixpoint DB_vshift (v : val) (cut : nat) {struct v}:=
   match v with
-  | Var id => if id == s_id then s_v else v
-  | Unit => v
-  | Int n => v
-  | Inl v' => Inl (vsubst σ v')
-  | Inr v' => Inr (vsubst σ v')
-  | Pair v1 v2 => Pair (vsubst σ v1) (vsubst σ v2)
-  | Fun id c => if id == s_id then v else Fun id (csubst σ c) 
-  | Handler ret_id c h => 
-      if ret_id == s_id then Handler ret_id c (hsubst σ h)
-      else Handler ret_id (csubst σ c) (hsubst σ h)
-  | VAnnot v ty =>  VAnnot (vsubst σ v) ty
+  | Var id => Var (id_shift id cut 1)
+  | Unit => Unit
+  | Int n => Int n
+  | Inl v' => Inl (DB_vshift v' cut)
+  | Inr v' => Inr (DB_vshift v' cut)
+  | Pair v1 v2 => Pair (DB_vshift v1 cut) (DB_vshift v2 cut)
+  | Fun x c => Fun x (DB_cshift c (cut+1))
+  | Handler x c_ret h =>
+      Handler x (DB_cshift c_ret (cut+1)) (DB_hshift h cut)
+  | VAnnot v' α => VAnnot (DB_vshift v' cut) α
   end
-with csubst (σ : var_id * val) (c : comp) :=
-  (* c [id |-> sv] *)
-  let (s_id, s_v) := σ in
+with DB_cshift (c : comp) (cut : nat) {struct c}:=
   match c with
-  | Ret v => Ret (vsubst σ v)
-  | ΠMatch v (id1, id2) c' => 
-      if id1 == s_id then c
-      else if id2 == s_id then c
-      else ΠMatch (vsubst σ v) (id1, id2) (csubst σ c')
-  | ΣMatch v idl cl idr cr =>
-      let cl' := if idl == s_id then cl else csubst σ cl in
-      let cr' := if idr == s_id then cr else csubst σ cr in
-      ΣMatch (vsubst σ v) idl cl' idr cr'
-  | App vf vx => App (vsubst σ vf) (vsubst σ vx)
-  | Op op_id v_arg id k =>
-      let k' := if id == s_id then k else csubst σ k in
-      Op op_id (vsubst σ v_arg) id k'
-  | LetRec f_id x_id c1 c2 =>
-      if f_id == s_id then c
-      else if x_id == s_id then c
-      else LetRec f_id x_id (csubst σ c1) (csubst σ c2)
-  | DoBind x_id c1 c2 =>
-      if x_id == s_id then DoBind x_id (csubst σ c1) c2
-      else DoBind x_id (csubst σ c1) (csubst σ c2)
-  | Handle v c' => Handle (vsubst σ v) (csubst σ c')
-  | CAnnot c ty => CAnnot (csubst σ c) ty
+  | Ret v => Ret (DB_vshift v cut)
+  | ΠMatch v (x, y) c =>
+      ΠMatch (DB_vshift v cut) (x, y) (DB_cshift c (cut+2))
+  | ΣMatch v xl cl xr cr =>
+      ΣMatch (DB_vshift v cut) xl (DB_cshift cl (cut+1))
+        xr (DB_cshift cr (cut+1))
+  | App v1 v2 => App (DB_vshift v1 cut) (DB_vshift v2 cut)
+  | Op op v_arg y c =>
+      Op op (DB_vshift v_arg cut) y (DB_cshift c (cut+1))
+  | LetRec f x c1 c2 =>
+      LetRec f x (DB_cshift c1 (cut+2)) (DB_cshift c2 (cut+1))
+  | DoBind x c1 c2 =>
+      DoBind x (DB_cshift c1 cut) (DB_cshift c2 (cut+1))
+  | Handle v c' => Handle (DB_vshift v cut) (DB_cshift c' cut)
+  | CAnnot c' C => CAnnot (DB_cshift c' cut) C
   end
-with hsubst (σ : var_id * val) (h : hcases) :=
-  (* c [id |-> sv] *)
-  let (s_id, s_v) := σ in
+with DB_hshift (h : hcases) (cut : nat) {struct h}:=
   match h with
-  | CasesØ => h
-  | CasesU h' op_id x_id k_id c_op =>
-      let c_op' := 
-        if x_id == s_id then c_op
-        else if k_id == s_id then c_op
-      else csubst σ c_op in
-      CasesU (hsubst σ h') op_id x_id k_id c_op'
-  end.
+  | CasesØ => CasesØ
+  | CasesU h op x k c => 
+      CasesU (DB_hshift h cut) op x k (DB_cshift c (cut+2))
+  end
+.
--- language_formalisation/type_system/bidirectional.v
+++ language_formalisation/type_system/bidirectional.v
@@ -18,8 +18,8 @@ with csynth : ctx -> comp -> ctype -> Type :=
 | SynthRet Γ v α : csynth Γ (Ret v) (CTy α SigØ EqsØ)
 | SynthPMatch Γ v α β x y c ctyC :
     vsynth Γ v (TyΠ α β) ->
-    csynth (CtxU x α (CtxU y β Γ)) c ctyC ->
-    csynth Γ c ctyC
+    csynth (CtxU (CtxU Γ β) α) c ctyC ->
+    csynth Γ (ΠMatch v (x, y) c) ctyC
 | SynthApp Γ v1 v2 α ctyC :
     vsynth Γ v1 (TyFun α ctyC) ->
     vcheck Γ v2 α ->
@@ -45,31 +45,31 @@ with vcheck : ctx -> val -> vtype -> Type :=
     vcheck Γ v2 β ->
     vcheck Γ (Pair v1 v2) (TyΠ α β)
 | CheckFun Γ x c α C :
-    ccheck (CtxU x α Γ) c C ->
+    ccheck (CtxU Γ α) c C ->
     vcheck Γ (Fun x c) (TyFun α C)
 | CheckHandler Γ x c_ret h α sig eqs D :
-    ccheck (CtxU x α Γ) c_ret D ->
+    ccheck (CtxU Γ α) c_ret D ->
     hcheck Γ h sig D ->
     vcheck Γ (Handler x c_ret h) (TyHandler (CTy α sig eqs) D)
 with ccheck : ctx -> comp -> ctype -> Type :=
 | CheckCBySynth Γ c C C' : csynth Γ c C' -> C = C' -> ccheck Γ c C
 | CheckSMatch Γ v α β xl cl xr cr C :
     vsynth Γ v (TyΣ α β) ->
-    ccheck (CtxU xl α Γ) cl C ->
-    ccheck (CtxU xr β Γ) cr C ->
+    ccheck (CtxU Γ α) cl C ->
+    ccheck (CtxU Γ β) cr C ->
     ccheck Γ (ΣMatch v xl cl xr cr) C
-| CheckOp Γ op_id v y c α_op β_op α sig eqs :
-    get_op_type sig op_id = Some (α_op, β_op) ->
+| CheckOp Γ op_id v y c α_op β_op α Σ eqs :
+    get_op_type Σ op_id = Some (α_op, β_op) ->
     vcheck Γ v α_op ->
-    ccheck (CtxU y β_op Γ) c (CTy α sig eqs) ->
-    ccheck Γ (Op op_id v y c) (CTy α sig eqs)
+    ccheck (CtxU Γ β_op) c (CTy α Σ eqs) ->
+    ccheck Γ (Op op_id v y c) (CTy α Σ eqs)
 with hcheck : ctx -> hcases -> sig -> ctype -> Type :=
 | CheckCasesØ Γ D : hcheck Γ CasesØ SigØ D
-| CheckCasesU Γ h op_id x k c_op α_op β_op sig D :
+| CheckCasesU Γ h op_id x k c_op α_op β_op Σ D :
     find_op_case h op_id = None ->
-    hcheck Γ h sig D ->
-    ccheck (CtxU x α_op (CtxU k (TyFun β_op D) Γ)) c_op D ->
+    hcheck Γ h Σ D ->
+    ccheck (CtxU (CtxU Γ (TyFun β_op D)) α_op) c_op D ->
     hcheck 
       Γ (CasesU h op_id x k c_op)
-      (SigU op_id α_op β_op sig) D
+      (SigU Σ op_id α_op β_op) D
 .
